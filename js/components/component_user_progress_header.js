a6feef50cd693dec887b6544bd3821ee457ea757
My Bergquist Rajic <mybergqvist@gmail.com>
2023-05-13 15:48:34 +0200

    Visual basics of header
    - Basic rendering and styling of header.
    - Added new div on row 46 in content_users.js

--- js/components/component_user_progress_header.js
+++ js/components/component_user_progress_header.js
@@ -1,11 +1,54 @@
 import state_io from "../utils/state_io.js";
 import { SubPub } from "../utils/subpub.js";
 
-export default {}
+export default {};
 
-;(() => {
-
-
-
-
+; (() => {
+    SubPub.subscribe({
+        event: "db::get::course::done",
+        listener: render
+    });
 })();
+
+
+function render() {
+    let progressHeader = document.getElementById("progress_header")
+
+    // Display ranking header (otherwise hidden until course has been chosen)
+    progressHeader.style.display = "";
+
+    progressHeader.innerHTML = `
+    <div id="progress_header_rank_img"></div>
+    <div id="progress_header_info">
+        <div id="progress_header_currentstreak">Current streak: 3</div>
+        <div id="progress_header_recentbadge">
+            <p>Recent badge:</p>
+            <div></div>
+        </div>
+    </div>
+    `
+
+}
+
+
+/*
+
+    let { bestStreak, badge, divison } = state_io.state.user; 
+
+    containerDiv 
+        divisonDiv
+            bildDivison
+        divInfo
+            innehåller treNycklarna
+            bild på badge 
+            pCurrentStreak
+            divProgressBar
+     
+    containerDiv append to content_course_open div i index.php
+
+    containerDiv lister click 
+        publicera
+            event: “render_user_progress”
+
+            
+*/
