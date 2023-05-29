6aca73a3381938119ef170785c97b737c0777421
Mehmet-Kaan <taspunar10@hotmail.com>
2023-05-29 11:16:16 +0200

    Delete component_ranking.js

--- js/components/component_ranking.js
+++
@@ -1,8 +0,0 @@
-import state_io from "../utils/state_io.js";
-import { SubPub } from "../utils/subpub.js";
-
-export default {};
-; (() => {
-})();
-
-// OBS!!!!! Flyttad till utils för att export skulle fungera!!!!!
